caabe9de725db0beb719c56d4536af1025ca3083
Yevhen <skaretskiy1999@gmail.com>
2023-09-11 12:21:36 +0300

    SignInButton was found

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import {useState, useEffect} from 'react';
 import {signOut, getProviders, useSession} from 'next-auth/react';
 
-import SignInButton from './signInButton';
+import SignInButton from './SignInButton';
 
 import {
 	BuiltInProviderType,
